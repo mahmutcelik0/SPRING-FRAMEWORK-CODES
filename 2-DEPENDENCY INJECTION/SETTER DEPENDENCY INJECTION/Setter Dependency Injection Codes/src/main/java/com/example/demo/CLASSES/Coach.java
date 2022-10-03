4420f28804b8070aa7a8c628526116da54a4194c
Mahmut ÇELİK <71070163+mahmutcelik0@users.noreply.github.com>
2022-10-03 10:00:12 +0300

    Add files via upload

---
+++ 2-DEPENDENCY INJECTION/SETTER DEPENDENCY INJECTION/Setter Dependency Injection Codes/src/main/java/com/example/demo/CLASSES/Coach.java
@@ -0,0 +1,6 @@
+package com.example.demo.CLASSES;
+
+public interface Coach {
+     String getDailyWorkout();
+     String getDailyFortune();
+}
---
+++ 2-DEPENDENCY INJECTION/SETTER DEPENDENCY INJECTION/Setter Dependency Injection Example/src/main/java/com/example/demo/HUMAN/Hearth.java
@@ -0,0 +1,13 @@
+package com.example.demo.HUMAN;
+
+public class Hearth implements ORGAN{
+    @Override
+    public String getDuty() {
+        return "ENSURING BLOOD CIRCULATION IN THE VESSELS";
+    }
+
+    @Override
+    public Integer getCount() {
+        return 1;
+    }
+}
