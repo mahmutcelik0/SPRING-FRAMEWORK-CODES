3cc2a993b75e6fea0219995d377ef1d9deb54c7b
Mahmut ÇELİK <71070163+mahmutcelik0@users.noreply.github.com>
2022-10-03 09:52:23 +0300

    Delete IOC directory

--- IOC/IoC Codes/src/main/java/com/example/demo/CoachClasses/BaseBallCoach.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.CoachClasses;
-
-public class BaseBallCoach implements Coach{
-    @Override
-    public String getDailyWorkOut() {
-        return "YOU HAVE TO THROW BALL LEAST 30 TIMES";
-    }
-}
--- IOC/IoC Codes/src/main/java/com/example/demo/CoachClasses/FootBallCoach.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.CoachClasses;
-
-public class FootBallCoach implements Coach{
-
-    @Override
-    public String getDailyWorkOut() {
-        return "YOU HAVE TO SHOOT BALL LEAST 10 TIMES";
-    }
-}
